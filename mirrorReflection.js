f6ad99d952ea76eb831fc11a6a1574f9dc793248
August Dolan <august.dolan1@gmail.com>
2020-11-17 13:08:29 -0500

    Implement mirrorReflection solution

--- mirrorReflection.js
+++ mirrorReflection.js
@@ -1,65 +1,24 @@
 var mirrorReflection = function(p, q) {
   let width = p;
   let height = q;
-  let slope = q / p;
-  let firstReceptor;
-  let right = false;
-  let up = true;
 
   while (true) {
-    if (width === p || width === 0) {
-      if (height === 0 && width === p) {
-        firstReceptor = 0;
-        break;
-      }
-      if (height === p && width === p) {
-        firstReceptor = 1;
-        break;
-      }
-      if (height === p && width === 0) {
-        firstReceptor = 2;
-        break;
-      }
-    }
 
-
-    if (up) {
-      height += slope;
-    } else {
-      height -= slope;
+  if (height % p === 0 && width % p === 0) {
+    if ((height / p) % 2 === 0 && (width / p) % 2 === 1) {
+      return 0;
     }
-
-    if (right) {
-      width += 1;
-    } else {
-      width -= 1
-    }
-
-    if (height >= p || height <= 0) {
-      if (height < 0) {
-        height = Math.abs(height);
-      } else {
-        height = p - (height - p);
-        if (height === p) {
-          up = !up;
-        }
+    if ((height / p) % 2 === 1) {
+      if ((width / p) % 2 === 1) {
+        return 1;
       }
-      up = !up;
-    }
-    if (width >= p || width <= 0) {
-      if (width < 0) {
-        width = Math.abs(width);
-        if (width === p) {
-          right = !right;
-        }
-      } else {
-        width = p - (width - p);
-      }
-      right = !right;
+      return 2;
     }
   }
 
-  return firstReceptor
-};
+  width += p;
+  height += q;
+  }
+}
 
 console.log(mirrorReflection(2, 1))
